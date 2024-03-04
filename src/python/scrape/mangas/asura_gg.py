75c35d393bd660996ec4de4031c66302e8c40bda
mrel <pepperonipiza@hotmail.com>
2024-03-04 17:06:38 +0100

    Try to redirect old urls

--- src/python/scrape/mangas/asura_gg.py
+++ src/python/scrape/mangas/asura_gg.py
@@ -1,45 +1,45 @@
 import requests
-from helpers.story_type import StoryType
 from helpers.driver import Driver
-from scrape.basic_scraper import BasicConfiguration;
-from scrape.configure_site_scraper import ConfigureSiteScraper;
-from selectolax.parser import Node
-from helpers.scraper_result import KeyResult, UrlResult;
+from scrape.mangas.asuratoon_com import SiteScraper as Asuratoon;
 
-class SiteScraper(ConfigureSiteScraper):
+class SiteScraper(Asuratoon):
     def __init__(self, url: str, driver: Driver, session_dict: dict[str, requests.Session]):
-        # super().useHtml(url);
-        super().useDriver(url, driver);
-        # super().useReDriver(url, driver);
-        # super().useSession(url, session_dict);
+        super().__init__(url.replace('asura.gg', 'asuratoon.com', 1), driver, session_dict);
+
+# class SiteScraper(ConfigureSiteScraper):
+    # def __init__(self, url: str, driver: Driver, session_dict: dict[str, requests.Session]):
+    #     # super().useHtml(url);
+    #     super().useDriver(url, driver);
+    #     # super().useReDriver(url, driver);
+    #     # super().useSession(url, session_dict);
         
-    def getConfiguration(self, url: str):
-        return BasicConfiguration(
-            get_story_type = lambda node, sections: StoryType.MANGA,
-            src = 'src',
-            get_chapter = lambda node, sections: node.css_first('#readerarea'),
-            get_titles = lambda node, sections: self.get_titles(node, sections),
-            get_urls = lambda node, sections: self.get_urls(node, sections, url),
-            get_keys = lambda node, sections: KeyResult(
-                story = "-".join(sections[3].split('-')[:-2]),
-                chapter = "-".join(sections[3].split('-')[-2:]),
-                domain = None,
-            ),
-        );
+    # def getConfiguration(self, url: str):
+    #     return BasicConfiguration(
+    #         get_story_type = lambda node, sections: StoryType.MANGA,
+    #         src = 'src',
+    #         get_chapter = lambda node, sections: node.css_first('#readerarea'),
+    #         get_titles = lambda node, sections: self.get_titles(node, sections),
+    #         get_urls = lambda node, sections: self.get_urls(node, sections, url),
+    #         get_keys = lambda node, sections: KeyResult(
+    #             story = "-".join(sections[3].split('-')[:-2]),
+    #             chapter = "-".join(sections[3].split('-')[-2:]),
+    #             domain = None,
+    #         ),
+    #     );
 
-    def get_titles(self, node: Node, sections: list[str]):
-        chapter = node.css_first('h1.entry-title')
-        return KeyResult(
-            chapter = chapter.text(),
-            domain = None,
-            story = None,
-        );
+    # def get_titles(self, node: Node, sections: list[str]):
+    #     chapter = node.css_first('h1.entry-title')
+    #     return KeyResult(
+    #         chapter = chapter.text(),
+    #         domain = None,
+    #         story = None,
+    #     );
 
-    def get_urls(self, node: Node, sections: list[str], url: str):
-        prev = node.css_first('.nextprev a.ch-prev-btn');
-        next = node.css_first('.nextprev a.ch-next-btn');
-        return UrlResult(
-                prev = self.tryGetHref(prev),
-                current = url,
-                next = self.tryGetHref(next),
-        );
+    # def get_urls(self, node: Node, sections: list[str], url: str):
+    #     prev = node.css_first('.nextprev a.ch-prev-btn');
+    #     next = node.css_first('.nextprev a.ch-next-btn');
+    #     return UrlResult(
+    #             prev = self.tryGetHref(prev),
+    #             current = url,
+    #             next = self.tryGetHref(next),
+    #     );
